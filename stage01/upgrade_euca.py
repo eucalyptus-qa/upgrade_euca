87fa4e764d4d2ed2787358d55e0524a43f8d556a
Kyo Lee <kyo.lee@eucalyptus.com>
2012-09-08 13:30:41 -0700

    forgot ./api/1 line on REPO_API

--- stage01/upgrade_euca.py
+++ stage01/upgrade_euca.py
@@ -6,7 +6,7 @@ import urllib
 
 # Hard-coded for now
 #REPO_API="http://192.168.51.243:5000/genrepo"
-REPO_API="http://packages.release.eucalyptus-systems.com/genrepo"
+REPO_API="http://packages.release.eucalyptus-systems.com/api/1/genrepo"
 
 def debian_package_upgrade(host):
     ret = 0
